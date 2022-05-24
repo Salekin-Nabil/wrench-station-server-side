5fe44db560711f5d8f736b9b5ef8677f95e11c46
Salekin-Nabil <salekinnabil@gmail.com>
2022-05-25 00:16:00 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Server is running swiftly.");
+});
+
+app.listen(port, ()=>{
+    console.log("Port no: ", port);
+});
