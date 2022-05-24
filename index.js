58155a54ca07ebb8e96aff482b7170a6b0466a5d
Salekin-Nabil <salekinnabil@gmail.com>
2022-05-25 00:28:23 +0600

    Get API to get 6 Products

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0dn1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        const productCollection = client.db('wrench-station').collection('product');
+
+        //Products GET API 6 Items
+        app.get('/products_6', async (req, res)=>{
+            const cursor = productCollection.find({}).limit(6);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Server is running swiftly.");
