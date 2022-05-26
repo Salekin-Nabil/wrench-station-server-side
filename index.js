c121fe662f18c8f15b21d00cf4490e16e1c55d47
Salekin-Nabil <salekinnabil@gmail.com>
2022-05-26 15:09:39 +0600

    Get API My Orders

--- index.js
+++ index.js
@@ -9,22 +9,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'Unauthorized access' });
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(403).send({ message: 'Forbidden access'});
+//         }
+//         console.log('decoded', decoded);
+//         req.decoded = decoded;
+//         next();
+//     })
+
+// }
+
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.status(401).send({ message: 'Unauthorized access' });
+      return res.status(401).send({ message: 'UnAuthorized access' });
     }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden access'});
-        }
-        console.log('decoded', decoded);
-        req.decoded = decoded;
-        next();
-    })
-
-}
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0dn1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -58,6 +73,20 @@ async function run() {
             const product = await productCollection.findOne(query);
             res.send(product);
         });
+        //Orders GET API My Orders
+        app.get('/myOrders', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const buyer = req.query.buyer;
+            if (buyer === decodedEmail) {
+                const query = { buyerEmail: buyer };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
+        })
         //Orders POST API
         app.post('/orders', async (req, res)=>{
             const newOrder = req.body;
